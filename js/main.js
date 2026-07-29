b39c48cf1e52c4c03004141ead8dd41899db64e1
kisi toledo <kitoledo@egresados.ubiobio.cl>
2026-07-29 15:48:52 -0400

    feat: integración final de main.js y API

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// (este será mi ejemplo de instanciacion provisional, me servirá para probar que todo lo hecho funciona que luego comentaré)
+// (este será mi ejemplo de instanciacion provisional, me servirá para probar que todo lo hecho funciona, luego lo comentaré)
 import { GestorTareas } from "./GestorTareas.js";
 import { UI } from "./ui.js";
+import { obtenerTareasAPI, recuperarDeLocalStorage } from "./api.js";
 
-/* ========================================================
+/* 
    PRUEBAS INICIALES (Puntos 1 y 2)
-   --------------------------------------------------------
    Este código lo usé para validar por consola que la POO 
    y los métodos del Gestor funcionaban correctamente.
    
@@ -16,20 +16,35 @@ import { UI } from "./ui.js";
    tarea1.cambiarEstado('completada');
    gestorDePrueba.eliminarTarea(tarea2.id);
    console.log('Tareas tras modificaciones:', gestorDePrueba.obtenerTareas());
-======================================================== */
+*/
 
-// ========================================================
 // CÓDIGO OPERATIVO DE LA APLICACIÓN (Paso 3 en adelante)
-// ========================================================
 
-// 1. Instanciamos la clase que maneja los datos
+// 1. Instanciar la clase que maneja los datos
 const gestor = new GestorTareas();
 
-// 2. Instanciamos la clase que maneja lo visual y le pasamos el gestor
+// 2. Instanciar la clase que maneja lo visual y le pasamos el gestor
 const ui = new UI(gestor);
 
-// 3. Encendemos los eventos del DOM (clicks, submits, teclado)
-ui.iniciarEventos();
+// FUNCION ASÍNCRONA DE INICIALIZACION (PASO 5)
+const iniciarApp = async () => {
+    // 5a. aqui intentamos recuperar los datos del navegador (LocalStorage)
+    let tareas = recuperarDeLocalStorage();
 
-// 4. Renderizamos la lista inicial (vacía al cargar)
-ui.renderizar();
+    // 5b. aqui si es la primera vez que entra y no hay datos, consumimos la API
+    if (tareas.length === 0) {
+        tareas = await obtenerTareasAPI();
+    }
+    
+    // 5c. aqui se le pasan los datos al gestor
+    gestor.cargarTareas(tareas);
+
+    // 3. Encendemos los eventos del DOM (clicks, submits, teclado) 
+    ui.iniciarEventos();
+
+    // 4. Renderizamos la lista inicial
+    ui.renderizar();
+}; 
+
+// 5d. Arrancamos la aplicacion
+iniciarApp();
